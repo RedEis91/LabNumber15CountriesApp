5098b667c326504a283d624a6569199164ece601
RedEis91 <imitationkrabs91@gmail.com>
2017-07-26 15:55:54 -0400

    lab 15 done and done

---
+++ src/CountriesApp.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+import java.io.*;
+
+/**
+ * Created by Jared on 7/26/17.
+ */
+public class CountriesApp {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int keepGoing;
+        CountriesTextFile countries = new CountriesTextFile();
+        File myFile = countries.myFile;
+
+
+        System.out.println("Welcome to the Countries Maintenance Application!");
+        System.out.println("What's your name?");
+        String name = scan.nextLine();
+        System.out.println("Hi " + name +"!");
+        System.out.println("Please select option 1,2 or 3: ");
+        do {
+        keepGoing = menu(scan);
+        if (keepGoing == 1) {
+            countries.readFromFile(myFile);
+
+        }
+        if (keepGoing == 2) {
+            System.out.print("Please enter a country: ");
+            scan.nextLine();
+            String input = scan.nextLine();
+            System.out.println("You added " + input + " to the list. Thanks "+ name + "!");
+            countries.writeToFile(input, myFile);
+
+        }
+        if (keepGoing < 1 || keepGoing > 3) {
+            System.out.println("*Invalid selection*" + "\nPlease select 1, 2 or 3");
+                keepGoing = menu(scan);
+        }
+
+        } while (keepGoing != 3);
+        System.out.println("Thanks for stopping by, "+name+ "! \nSafe travels!");
+    }
+
+    private static int menu(Scanner scan) {
+        int keepGoing;
+        System.out.println("1. Display List of Countries ");
+        System.out.println("2. Add A Country ");
+        System.out.println("3. Exit");
+        keepGoing = scan.nextInt();
+        return keepGoing;
+    }
+}
---
+++ src/CountriesTextFile.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.Scanner;
+
+/**
+ * Created by Jared on 7/26/17.
+ */
+public class CountriesTextFile {
+    File myFile = new File("countries.txt");
+
+    public static void writeToFile(Object input, File myFile) {
+        //tank to hold file
+
+        Scanner scan = new Scanner(System.in);
+        //any error the program can't handle at runtime is known as an Exception
+        //PrintWriter class has method built in to say: "Hey, I know you're going to be looking for a file"
+        //In case you don't have access to that file, for whatever reason...
+        //surround your print-writer with a try/catch to catch the exception
+        try {
+            //pipe  to stream file into tank             //pump, which by default deletes data from file
+            PrintWriter p = new PrintWriter(new FileOutputStream(myFile, true));
+            //            p.println("First line!!! Wohooo!");
+            p.println(input);//save user input to the file
+            p.close();
+            //need to handle the FileNotFoundException
+        } catch (IOException e) {
+            System.out.println("File not found!!!!");
+            e.printStackTrace();
+            System.out.println(e);
+        }
+    }
+    public static void readFromFile(File myFile) {
+        //FileReader always needs to be wrapped in Try/Catch.
+        try {
+            // to use buffered reader, you have to instantiate a reader first
+            FileReader reader = new FileReader(myFile);
+            //BufferedReader (more specific file reader) has method called readLine() to read data stream line by line
+            BufferedReader bufReader = new BufferedReader(reader);
+            String line = bufReader.readLine();
+            //ensure the line being fed into bufReader isn't null
+            while (line != null) {
+                System.out.println(line);
+                line = bufReader.readLine(); // read the next line
+            }
+            bufReader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("File not found!");
+        }
+    }
+
+}
